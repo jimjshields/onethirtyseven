f1d4b40729b1c0a4066de84d9c2c846d54207105
Jim Shields <jim.j.shields@gmail.com>
2014-10-13 22:16:32 -0400

    Added urls to interactive

---
+++ interactive/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url
+
+from blog import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_137to1.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name='index'),
+    url(r'^movie_page/', views.movie_page, name='movie_page'),
+    url(r'^base_template/', views.base_template, name='base_template'),
+    
+)
