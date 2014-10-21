03d7d9f342509b845b2560086ee39fdd79583e00
Jim Shields <jim.j.shields@gmail.com>
2014-10-21 14:09:03 -0400

    Changed static files directory

--- onethirtyseven/settings.py
+++ onethirtyseven/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -83,7 +84,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
